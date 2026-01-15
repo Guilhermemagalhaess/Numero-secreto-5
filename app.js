f7dd83a0c90d3dedaadf6e4a15e40b02749c2042
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 13:25:56 -0300

    Enquanto

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 alert ("Boas vindas ao jogo do número secreto edição 5");
 let numeroSecreto = 7;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 10");
+console.log(numeroSecreto);
+let chute; 
 
-
+//enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+   chute = prompt("Escolha um número entre 1 e 10");
 //Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai, vocÊ descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert (`O número secreto é menor que ${chute}`);
+    if (chute == numeroSecreto) {
+        alert(`Isso ai, vocÊ descobriu o número secreto ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
-    }
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
 
+    }
 }
