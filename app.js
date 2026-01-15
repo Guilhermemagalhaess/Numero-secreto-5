01cfe0b5e2b8d9f0eeebe4856cdca1ae952b4d0c
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 13:32:37 -0300

    Tentativas

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ alert ("Boas vindas ao jogo do número secreto edição 5");
 let numeroSecreto = 7;
 console.log(numeroSecreto);
 let chute; 
+let tentativas = 1;
 
 //enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
    chute = prompt("Escolha um número entre 1 e 10");
 //Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai, vocÊ descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai, vocÊ descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas. `);
     } else {
         if (chute > numeroSecreto) {
             alert (`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
 }
