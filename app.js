e60cdda3e368af7b58cc01d4bc7934afd46e3ed3
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 13:15:32 -0300

    Novas condições

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-alert ("Sejam bem vindos ao jogo do número sereto edição 5");
-let  numeroSecreto = 12;
-console.log (numeroSecreto)
-let chute = prompt ("digite um número entre 1 e 30");
+alert ("Boas vindas ao jogo do número secreto edição 5");
+let numeroSecreto = 7;
+console.log(numeroSecreto)
+let chute = prompt("Escolha um número entre 1 e 10");
 
- //  se o chute for igual ao número secreto
-if (numeroSecreto == chute) {
-   alert (`Isso ai! Você acertou o número secreto ${numeroSecreto}`);
-} else  {
-    alert ("Você errou")
+
+//Se o chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai, vocÊ descobriu o número secreto ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto) {
+        alert (`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
+
 }
-
