9796ba2489c6622e66e1ea4c85d84fb98623f043
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 08:48:38 -0300

    Comparando variáveis

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert ("Bem vindo ao jogo do número secreto edição 5");
 let  numeroSecreto = 29;
-prompt ("Escolha um número entre 1 e 10");
+let chute = prompt ("Escolha um número entre 1 e 30");
+
+    if (chute == numeroSecreto) {
+    console.log ("Isso ai, você acertou o número secreto (29");
+}
