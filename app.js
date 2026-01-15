4c013e02085fd580d3be53d39cd8202b2454f94a
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 10:45:32 -0300

    Else e comentários

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-alert ("Boas vindas site!");
-let  saldoDisponivel = 1000;
-let chute = prompt ("Saldo disponível");
+alert ("Sejam bem vindos ao jogo do número sereto edição 5")
+let  numeroSecreto = 18;
+console.log (numeroSecreto)
+let chute = prompt ("digite um número entre 1 e 30");
 
-    if (saldoDisponivel == chute) {
-    console.log ("Isso ai, você acertou");
-} else {
-    alert ("VocÊ errou.")
+ //  se o chute for igual ao número secreto
+if (numeroSecreto == chute) {
+    
+    alert ("Você acertou");
+} else  {
+    alert ("Você errou")
 }
+
