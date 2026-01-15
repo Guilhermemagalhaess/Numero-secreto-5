e9b69e7a4f7d38dd8000b56e650d9f1e15c38e2d
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-15 11:08:56 -0300

    Concatenação

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-alert ("Sejam bem vindos ao jogo do número sereto edição 5")
-let  numeroSecreto = 18;
+alert ("Sejam bem vindos ao jogo do número sereto edição 5");
+let  numeroSecreto = 12;
 console.log (numeroSecreto)
 let chute = prompt ("digite um número entre 1 e 30");
 
  //  se o chute for igual ao número secreto
 if (numeroSecreto == chute) {
-    
-    alert ("Você acertou");
+   alert (`Isso ai! Você acertou o número secreto ${numeroSecreto}`);
 } else  {
     alert ("Você errou")
 }
